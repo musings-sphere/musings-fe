f95c6f61c515b3fc53ff9c3b2df5f29868df7972
Francis Masha <francismasha96@gmail.com>
2021-04-05 13:01:58 +0300

    feat: add nextjs pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import IndexView from 'views/IndexView';
 import Main from 'layouts/Main';
 import WithLayout from 'WithLayout';
+import { BlogNewsroom } from '../src/views';
 
 const IndexPage = (): JSX.Element => {
   return (
     <WithLayout
-      component={IndexView}
+      component={BlogNewsroom}
       layout={Main}
     />
   )
